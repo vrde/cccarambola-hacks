29fb2b1aaef61fd951ca6fb1fded3318cf910ab5
vrde <agranzot@gmail.com>
2011-12-28 16:51:21 +0100

    moved *duino driver to its proper folder; updated python driver

--- demo/test.py
+++ demo/test.py
@@ -1,13 +1,8 @@
 import serial
 from random import randint, random
 
-from collections import deque
 
-r = '\x10\x00\x00'
-g = '\x00\x10\x00'
-b = '\x00\x00\x10'
-w = '\x10\x10\x10'
-k = '\x00\x00\x00'
+from collections import deque
 
 class Color(object):
     __slots__ = ('r', 'g', 'b')
@@ -24,13 +19,8 @@ class Color(object):
         return self.r or self.g or self.b
 
 class Driver(object):
-
-    def __init__(self, serial=None, factor=0.5, length=32):
-        if serial is None:
-            serial = serial.Serial('/dev/ttyUSB0', 115200,
-                    xonxoff=0, rtscts=0, timeout=None)
-
-        self.serial = serial
+    def __init__(self, s, factor=1, length=32):
+        self.serial = s
         self.factor = factor
         self.length = length
         self.strip = deque(Color(0, 0, 0) for i in range(length))
@@ -39,49 +29,58 @@ class Driver(object):
         f = self.factor
         self.serial.write('\254') #SYNC
         for c in self.strip:
-            self._write(chr(int(c.r * f)))
-            self._write(chr(int(c.g * f)))
-            self._write(chr(int(c.b * f)))
-
-    def __setitem__(self, pos, val):
-        assert len(val) == 3
-        self.strip[pos].r = val[0]
-        self.strip[pos].g = val[1]
-        self.strip[pos].b = val[2]
+                self._write(chr(int(c.r * f)))
+                self._write(chr(int(c.g * f)))
+                self._write(chr(int(c.b * f)))
 
     def _write(self, char):
-        if char == '\254':
-            char = '\255'
+        if char == "\254":
+            char = "\255"
         self.serial.write(char)
+
+        
+        
 
     def shift(self, n):
         self.strip.rotate(n)
 
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) == 2:
-        port = sys.argv[1]
-    else:
-        sys.exit('usage: stripdriver.py /dev/<tty-usb>')
-
-    s = serial.Serial(port, 115200, xonxoff=0, rtscts=0, timeout=None)
-    d = Driver(serial=s, factor=0.05)
+if __name__ == "__main__":
+    
+    
+    scale = 64
+    
+    r = '\x10\x00\x00'
+    g = '\x00\x10\x00'
+    b = '\x00\x00\x10'
+    w = '\x10\x10\x10'
+    k = '\x00\x00\x00'
+    
+    s = serial.Serial('/dev/ttyS1', 115200, xonxoff=0, rtscts=0, timeout=None)
+    # = ''.join(chr(randint(0, 16)) for i in range(3 * 32))
+    #o = b + k * 31
+    d = Driver(s, factor=0.05)
+    
+    import time
     
     d.strip[0].r = 255
     
     c = 0
     
     while True:
+        #time.sleep(0.05)
         if random() <= 0.1:
             if random() < 0.5:
                 kill = randint(0, 31)
                 if d.strip[kill]:
+                    #d.strip[kill] = Color(0,0,255)
+                    #d.render()
                     d.strip[kill] = Color(0, 0, 0)
             else:
                 spawn = randint(0, 31)
                 if not d.strip[spawn]:
+                    #d.strip[spawn] = Color(0 ,255, 0)
+                    #d.render()
                     d.strip[spawn] = Color(255, 0, 0)
+        print "shift"
         d.shift(1)
         d.render()
-
